3ca55dc759141c729629f79cf729b7a34a3d4e93
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-01 16:42:28 -0700

    fronte end Auth form done. Need to fix backend of login/register functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { useContext } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
+import { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Auth from './components/Auth'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Auth from "./components/Auth";
+
+import AuthContext from "./store/authContext";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className="App">
       <Header />
       <Routes>
-        
-        <Route path='/auth'  />
+        <Route path="/" element={ <Home /> } />
+        <Route path="/auth" element={ authCtx.token ? <Auth signedIn={true} /> : <Auth signedIn={false} /> }/>
+        <Route path="*" element={ <Navigate to='/'/> }/>
       </Routes>
       <Footer />
     </div>
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import { useState, useContext } from "react";
+import axios from "axios";
+import AuthContext from "../store/authContext";
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const wins = 0;
+  const losses = 0;
+  const draws = 0;
+  const [register, setRegister] = useState(true);
 
-export default Auth
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitHandler called");
+
+    const body = {
+      username,
+      email,
+      password,
+      firstname,
+      lastname,
+      wins,
+      losses,
+      draws,
+    };
+
+    console.log("BODY", body);
+    axios
+      .post(register ? `/api/register` : `/api/login`, body)
+      .then(({ data }) => {
+        console.log("AFTER AUTH", data);
+        authCtx.login(data.token, data.exp, data.userId);
+      })
+      .catch((theseHands) => {
+        console.log("error with registering/logging in", theseHands);
+        setPassword("");
+        setUsername("");
+        setEmail("");
+        setFirstname("");
+        setLastname("");
+      });
+  };
+
+  return (
+    <main id="Auth">
+      <h1>Welcome to Phoenix Shogi</h1>
+      <form className="form auth-form" onSubmit={submitHandler}>
+        <input
+          className="form-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="First name"
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Last name"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Confirm Password"
+          value={cPassword}
+          required
+          onChange={(e) => setCPassword(e.target.value)}
+        />
+        <button className="form-btn">{register ? `Sign up` : `Login`}</button>
+      </form>
+      <button className="form-btn" onClick={() => setRegister(!register)}>
+        Need to <span>{register ? `Login` : `Sign up`}</span>?
+      </button>
+    </main>
+  );
+};
+
+export default Auth;
