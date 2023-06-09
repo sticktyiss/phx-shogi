ef86fb928328c7c04332b88b1ae8482acfee181d
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-09 14:20:22 -0700

    Finished footer css.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,17 @@ import React from "react";
 import { BsFacebook } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { IconContext } from "react-icons";
+import './css/Footer.css'
 
 const Footer = () => {
   return (
     <footer>
-      <IconContext.Provider value={{className: 'footerIcons', title: 'Icons'}}>
-      <div className="contact">
         <h1>Contact Us</h1>
         <a href="mailto:phoenixshogi@gmail.com">phoenixshogi@gmail.com</a>
         <p>650-SHOGI-AZ (746-4429)</p>
-      </div>
-      <div>
         <a href="https://www.facebook.com/groups/phxshogi">
           <BsFacebook />
         </a>
-      </div>
-      </IconContext.Provider>
     </footer>
   );
 };
