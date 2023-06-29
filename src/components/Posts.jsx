f5d89abd3c1e3b0e5ebc8c726eec80f5433775dd
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-28 17:30:45 -0700

    Deleted unused css files and added favicons

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import './css/Posts.css'
 
 const Posts = () => {
-  const newsPosts = () => {
-    return (
+  return (
+    <main className="posts">
       <div className="postCard">
         <h2>New beginnings</h2>
         <div>
@@ -11,12 +11,24 @@ const Posts = () => {
         </div>
         <p>Welcome to Phoenix Shogi</p>
       </div>
-    )};
-
-  return (
-    <main className="posts">
-      <div>
-      {newsPosts()}
+      <div className="postCard">
+        <h2>Meet Up</h2>
+        <div>
+        <h4>- July 8th, 2023</h4> 
+        </div>
+        <p>📣 Hey fellow shogi enthusiasts! 🏯🎌
+<br/>
+It's time to put our skills to the test in an epic shogi showdown! 🤩🔥
+<br/>
+Join us for an exciting meetup this weekend! Here are the details:
+<br/>
+📅 Date: [Insert Date]
+🕒 Time: [Insert Time]
+📍 Location: [Insert Venue]
+<br/>
+Prepare your strategies and bring your A-game, because we're about to have an awesome time together! 🎉🤝
+<br/>
+See you there! 🏆👑</p>
       </div>
       </main>
   )
