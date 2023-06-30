0d2468cb55ea0f40762ff237fd50a5758d0d07c2
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-30 15:22:31 -0700

    Changed Footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { BsFacebook, BsDiscord } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { IconContext } from "react-icons";
-import './css/Footer.module.css'
+import "./css/Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
-        <h1>Contact Us</h1>
-        <a href="mailto:phoenixshogi@gmail.com">phoenixshogi@gmail.com</a>
-        <p>650-SHOGI-AZ (746-4429)</p>
-        <a href="https://www.facebook.com/groups/phxshogi">
+      <h1>Contact Us</h1>
+      <a href="mailto:phoenixshogi@gmail.com">phoenixshogi@gmail.com</a>
+      <p>650-SHOGI-AZ (746-4429)</p>
+      <div>
+        <a href="https://www.facebook.com/groups/phxshogi" target="_blank">
           <BsFacebook />
         </a>
-        <a href="">
+        <a href="https://discord.gg/UVEQH9YDEk" target="_blank">
           <BsDiscord />
         </a>
+      </div>
     </footer>
   );
 };
